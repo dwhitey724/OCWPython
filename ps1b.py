7b4a76e42edbc4140d9a1c902b3c9175a013cb52
Devin Whitford <devin.whitford@eero.com>
2018-06-07 17:00:42 -0700

    messed around on ps1b, still not working

--- ps1b.py
+++ ps1b.py
@@ -7,17 +7,12 @@ month = 1
 monthly_interest = annual_interest / 12
 monthly_payment = 10
 
+
 while month <= 12:
     original_balance = balance
     balance = round(balance * (1 + monthly_interest) - monthly_payment, 2)
+    print(balance)
     month += 1
-
-    if balance >= 0:
-        monthly_payment += 10
-        balance = original_balance
-        month = 1
-    else:
-        break
 
 print('RESULT')
 print('Monthly payment to pay off debt in 1 year: ' + str(monthly_payment))

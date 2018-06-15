eacbbe1acaff64bb79bef32813166443359d3736
Devin Whitford <devin.whitford@eero.com>
2018-06-15 13:44:53 -0700

    still not working.

--- ps3b.py
+++ ps3b.py
@@ -19,14 +19,14 @@ def comp_choose_word(hand, word_list):
     comp_words = get_perms(hand, HAND_SIZE)
     comp_word_scores = [] * len(comp_words)
 
-    # Get scores for all word permutations. If not a real word, score is 0
+    # Get scores for all word permutations. If real word, return word associated with max score. If not, return None
     for item in comp_words:
         if is_valid_word(item, hand, word_list):
             comp_word_scores[item] = get_word_score(item, HAND_SIZE)
             return comp_words[
                 comp_word_scores.index((max(comp_word_scores)))]  # Should return value at index of max score
         else:
-            return 0
+            return None
 
 
 #
@@ -51,21 +51,24 @@ def comp_play_hand(hand, word_list, score):
      hand: dictionary (string -> int)
      word_list: list (string)
     """
-    total_score = 0
+
     print('Current Hand: ' + display_hand(hand))
     print('')
 
     comp_word = comp_choose_word(hand, word_list)
-    comp_word_score = get_word_score(comp_word, HAND_SIZE)
-    total_score += comp_word_score
-    print("'{0}' earned {1} points. Total: {2} points".format(comp_word, comp_word_score, total_score))
-    new_hand = update_hand(hand, comp_word)
+
+    if comp_word is not None:
+        comp_word_score = get_word_score(comp_word, HAND_SIZE)
+        score += comp_word_score
+        print("'{0}' earned {1} points. Total: {2} points".format(comp_word, comp_word_score, score))
+        new_hand = update_hand(hand, comp_word)
+        comp_play_hand(new_hand, word_list, score)
+
+    else:
+        print('Total Score: ' + str(score) + ' points')
+        print('')
 
 
-
-
-
-    
 #
 # Problem #6C: Playing a game
 #
@@ -88,8 +91,65 @@ def play_game(word_list):
 
     word_list: list (string)
     """
-    # TO DO...
-        
+    score = 0
+    count = 0
+
+    new_hand = deal_hand(HAND_SIZE)
+    last_hand = new_hand.copy()
+
+    def first_choice():
+        first_choice = input('Please input "n" to play new hand, "r" to replay last hand, or "e" to exit the game: ')
+        return first_choice
+
+    def second_choice():
+        second_choice = input('Please input "u" for user to play the game and "c" for computer to play the game: ')
+        return second_choice
+
+    while first_choice() is not 'e':
+        if first_choice() == 'e':
+            break
+
+        elif first_choice() == 'n':
+
+            if second_choice() == 'u':
+                count += 1
+                new_hand = deal_hand(HAND_SIZE)  # Deal new hand, use that
+                last_hand = new_hand.copy()  # Store hand as last hand to replay
+                play_hand(new_hand, word_list, score)
+                first_choice()
+
+            elif second_choice() == 'c':
+                count += 1
+                new_hand = deal_hand(HAND_SIZE)  # Deal new hand, use that
+                last_hand = new_hand.copy()  # Store hand as last hand to replay
+                comp_play_hand(new_hand, word_list, score)
+                first_choice()
+
+            else:
+                print('You have chosen an invalid option. Please try again.')
+                second_choice()
+
+        elif first_choice() == 'r' and count != 0:
+            second_choice()
+
+            if second_choice() == 'u':
+                play_hand(last_hand, word_list, score)
+                last_hand = new_hand.copy()
+                first_choice()
+
+            elif second_choice() == 'c':
+                comp_play_hand(last_hand, word_list, score)
+                last_hand = new_hand.copy()
+                first_choice()
+
+        elif first_choice() == 'r' and count == 0:
+            print('You have no hands to replay!')
+            first_choice()
+
+        else:
+            print('You have chosen an invalid option. Please try again.')
+            first_choice()
+
 #
 # Build data structures used for entire session and play game
 #
@@ -97,4 +157,3 @@ if __name__ == '__main__':
     word_list = load_words()
     play_game(word_list)
 
-    
